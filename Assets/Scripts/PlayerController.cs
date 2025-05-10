1e338b4752298b4a9a528c20577e9a40bc0199c7
DESKTOP-F68QGP8\VuTrong <Trongvuhuu03@gmail.com>
2025-05-10 18:50:30 +0700

    END LESSSON 03

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.Rendering.VirtualTexturing;
+
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]public int speed; //Dùng để thay đổi tốc độ
+    public Vector3[] point;
+    private void Start()
+    {
+        Debug.Log(speed);
+    }
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ShowLog.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System;
+
+public class ShowLog : MonoBehaviour
+{
+
+
+    private void Start()
+    {
+        // Gọi sau hàm Awake và OnEnable và trước khi nó cập nhật khung hình đầu tiên
+        Debug.Log("Start");
+    }
+
+
+    private void Update()
+    {
+        Debug.Log("Update");
+    }
+}
+
+    
