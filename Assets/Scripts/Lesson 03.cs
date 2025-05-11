1f391f1009b9ca2806374a34170f0e0056df5356
DESKTOP-F68QGP8\VuTrong <Trongvuhuu03@gmail.com>
2025-05-11 18:33:52 +0700

    Bổ sung code bài trước

---
+++ Assets/Scripts/Lesson 03.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+
+public enum GameState
+{
+    MainMenu,
+    IsGame,
+    Paused,
+    GameOver,
+}
+
+
+public enum DayOfWeek
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+
+}
+public class Lesson03 : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+
+    private void Update()
+    {
+        //Tính khoảng cách từ PointA  và Point B
+        float Distance = (pointA.transform.position - pointB.transform.position).magnitude;
+        Debug.Log("Khoảng cách giữ 2 điểm A và B: " + Distance);
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,20 +2,78 @@
 using System;
 using System.Collections.Generic;
 using System.Collections;
-using UnityEngine.Rendering.VirtualTexturing;
+
 
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]public int speed; //Dùng để thay đổi tốc độ
+    public enum TargetEnum
+    {
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight,
+    }
+    [SerializeField] public float speed; //Dùng để thay đổi tốc độ
+
+    public Transform topLeftTarget;
+    public Transform topRightTarget;
+    public Transform bottomLeftTarget;
+    public Transform bottomRightTarget;
+
+    private Transform currentTarget;
+    private TargetEnum nextTarget = TargetEnum.TopLeft; //Trạng thái đầu tiên;
     public Vector3[] point;
+    public GameObject player;
     private void Start()
     {
         Debug.Log(speed);
+        currentTarget = topLeftTarget;
+
     }
 
     private void Update()
     {
-        
+        Vector3 targetPostion = currentTarget.position;
+        Vector3 moveDirection = targetPostion - transform.position;
+
+        float distance = moveDirection.magnitude;
+
+        if (distance > 0.1f) //Khi chưa tới thì vẫn di chuyển đến focus
+        {
+            transform.position = Vector3.MoveTowards(transform.position, currentTarget.position, speed * Time.deltaTime);
+        }
+
+        else
+        {
+            //chuyển Target
+            SetNextTarget(nextTarget);
+        }
+
+        Vector3 direction = currentTarget.position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = targetRotation;
+    }
+    private void SetNextTarget(TargetEnum target)
+    {
+        switch (target)
+        {
+            case TargetEnum.TopLeft:
+                currentTarget = topLeftTarget;
+                nextTarget = TargetEnum.TopRight;
+                break;
+            case TargetEnum.TopRight:
+                currentTarget = topRightTarget;
+                nextTarget = TargetEnum.BottomLeft;
+                break;
+            case TargetEnum.BottomLeft:
+                currentTarget = bottomLeftTarget;
+                nextTarget = TargetEnum.BottomRight;
+                break;
+            case TargetEnum.BottomRight:
+                currentTarget = bottomRightTarget;
+                nextTarget = TargetEnum.TopLeft;
+                break;
+        }
     }
 }
